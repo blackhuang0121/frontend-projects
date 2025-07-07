a5c75f7d56fb2f1bac739c82d51a2ae9db81de6d
Howard Huang <blackhuang0121@gmail.com>
2025-07-07 20:26:29 +0800

    Add Metadata and posts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
         <header className="py-8 text-center border-b bg-neutral-900">
-            <h1 className="text-3xl font-bold mb-2">Frontend Projects made by Howard Huang</h1>
-            <h2 className="text-lg">持續更新中：這裡會收錄我的所有前端練習專案</h2>
+            <h1 className="text-3xl font-bold mb-2">Frontend Projects Created by Howard Huang</h1>
+            <h2 className="text-lg">不定期更新中：收錄練習過的前端練習專案</h2>
         </header>
     );
 }
