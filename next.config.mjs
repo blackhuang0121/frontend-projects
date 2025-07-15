ca1abd4f351a0cf0cb0b2f62024c009771bac5eb
Howard Huang <blackhuang0121@gmail.com>
2025-07-15 16:43:35 +0800

    Adjust image-slider project to react v3

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
-
-// next.config.js
-module.exports = {
+// next.config.mjs (ESM)
+export default {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'live.staticflickr.com',
-            },
+        domains: [
+            "live.staticflickr.com",
         ],
     },
-}
+};
